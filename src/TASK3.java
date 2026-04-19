c0fc77191d9f10e21fac17c88ed4b6d9215f639a
Shahd <shahad9mubarak@gmail.com>
2026-04-19 10:32:22 +0400

    creat loop structure

--- src/TASK3.java
+++ src/TASK3.java
@@ -1,6 +1,8 @@
 public class TASK3 {
     static void main(String[] args) {
        String catdogappear="catdog";
-
+       int catCount = 0;
+       int dogCount=0;
+       for (int i = 0 ; i<catdogappear.length()-1 ; i++){
     }
 }
