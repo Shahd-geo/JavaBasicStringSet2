abaa20346e52689b771d7ab8f83c6850ef02d562
Shahd <shahad9mubarak@gmail.com>
2026-04-19 12:36:56 +0400

    TASK 15 Return substring between first and last BREAD

--- src/TASK15.java
+++ src/TASK15.java
@@ -1,9 +1,18 @@
 public class TASK15 {
     static void main(String[] args) {
+        String str = "breadjambread";
+        System.out.println(getSandwich(str));
 
     }
     public static String getSandwich(String str){
         int first = str.indexOf("bread");
         int last = str.lastIndexOf("bread");
+        if (first != -1 && last != -1 && first != last) {
+
+            return str.substring(first + 5, last);
+        }
+
+        return "";
     }
+
 }
