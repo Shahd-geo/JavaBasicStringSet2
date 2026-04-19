a90a69dd97bc62bd43f46087789d4a2bca2c124f
Shahd <shahad9mubarak@gmail.com>
2026-04-19 11:23:07 +0400

    task 6 xyz There done

--- src/TASK6.java
+++ src/TASK6.java
@@ -1,11 +1,18 @@
 public class TASK6 {
     static void main(String[] args) {
     String str="abcxyz";
+    System.out.println(xyzThere(str));
+
 
     }
     public static boolean xyzThere(String str){
         for (int i = 0 ; i < str.length()-1 ; i++){
-            if ()
+            if (str.substring(i , i+3).equals("xyz")){
+                if (i == 0 || str.charAt(i - 1) != '.'){
+                    return true;
+                }
+            }
         }
+        return false;
     }
 }
