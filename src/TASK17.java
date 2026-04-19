4e247b47ac16aabe9cb896530fd7dc764e2a6c54
Shahd <shahad9mubarak@gmail.com>
2026-04-19 14:45:29 +0400

    doing the loop and assign to its value

--- src/TASK17.java
+++ src/TASK17.java
@@ -1,5 +1,17 @@
 public class TASK17 {
     static void main(String[] args) {
+        String str="abc";
 
+    }
+
+    public static String oneTwo(String str) {
+        String result = "";
+        for (int i = 0; i <= str.length() - 1; i += 3) {
+            result += str.charAt(i + 1);
+            result += str.charAt(i + 2);
+            result += str.charAt(i);
+
+        }
+        return result;
     }
 }
