030a646db4551b1c0823f3b3fb82d6625d75c501
Shahd <shahad9mubarak@gmail.com>
2026-04-19 11:17:39 +0400

    bulding loop iteration

--- src/TASK6.java
+++ src/TASK6.java
@@ -1,5 +1,11 @@
 public class TASK6 {
     static void main(String[] args) {
+    String str="abcxyz";
 
+    }
+    public static boolean xyzThere(String str){
+        for (int i = 0 ; i < str.length()-1 ; i++){
+            if ()
+        }
     }
 }
