f4213df39e5f555e35e68d1d81bd8e37f8b8f81d
Shahd <shahad9mubarak@gmail.com>
2026-04-19 15:14:05 +0400

    Task19 Remove '*' and adjacent characters

--- src/TASK19.java
+++ src/TASK19.java
@@ -1,5 +1,7 @@
 public class TASK19 {
     static void main(String[] args) {
+        String str = "ab*cd";
+        System.out.println(starOut(str));
 
     }
     public static String starOut(String str){
@@ -7,10 +9,11 @@ public class TASK19 {
         for (int i = 0 ; i <str.length();i++){
             if (str.charAt(i)=='*')continue;
             if (i > 0 && str.charAt(i-1)=='*')continue;
-            if (i < str.length()&&str.charAt(i+1)=='*')continue;
+            if (i < str.length()-1 &&str.charAt(i+1)=='*')continue;
             result+=str.charAt(i);
 
         }
+        return result;
 
     }
 }
