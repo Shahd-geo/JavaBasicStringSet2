5a7db4324d9396c177af2249e69885be835dd9f5
Shahd <shahad9mubarak@gmail.com>
2026-04-19 14:25:35 +0400

    start solving task16

---
+++ src/TASK16.java
@@ -0,0 +1,17 @@
+public class TASK16 {
+    static void main(String[] args) {
+        String str = "xy*yzz";
+        System.out.println();
+
+    }
+    public static boolean sameStarChar(String str){
+        for(int i =0 ; i <=str.length()-1;i++){
+            if (str.charAt(i)=='*'){
+                if (str.charAt(i-1)!=str.charAt(i+1)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
