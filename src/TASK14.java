a5fa7ffec5854ef8386132d61403f30eac3108dc
Shahd <shahad9mubarak@gmail.com>
2026-04-19 12:24:17 +0400

    start solving task14

--- src/TASK14.java
+++ src/TASK14.java
@@ -1,5 +1,11 @@
 public class TASK14 {
     static void main(String[] args) {
 
+
+    }
+    public static boolean xyzMiddle(String str){
+        for (int i = 0 ; i<str.length()-1 ; i++){
+            if (str.substring())
+        }
     }
 }
