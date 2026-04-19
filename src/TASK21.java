b3e56557bd86f9877aec5f470e9059371faed0e9
Shahd <shahad9mubarak@gmail.com>
2026-04-19 15:32:16 +0400

    loop through string

---
+++ src/TASK21.java
@@ -0,0 +1,13 @@
+public class TASK21 {
+    static void main(String[] args) {
+
+    }
+    public static String wordEnds(String str, String word) {
+
+        String result = "";
+
+        for (int i = 0; i <= str.length() - word.length(); i++) {
+
+            if (str.substring(i, i + word.length()).equals(word)) {
+
+            }
