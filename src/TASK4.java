def256af1d0660057dad3bdb032c874a0c20efbf
Shahd <shahad9mubarak@gmail.com>
2026-04-19 10:54:13 +0400

    doing the loop iteration

--- src/TASK4.java
+++ src/TASK4.java
@@ -1,5 +1,13 @@
 public class TASK4 {
     static void main(String[] args) {
+        String TEXT = "aaacodebbb";
+        int count = 0;
+        for (int i = 0 ; i < TEXT.length();i++){
+            if (TEXT.charAt(i))=='c'&& TEXT.charAt(i +1)=='o'&&TEXT.charAt(i+3)=='e'){
+                count++
 
+            }
+
+        }
     }
 }
