3c89a7aefdc69f175acb6595c6e17a14fb00909b
Shahd <shahad9mubarak@gmail.com>
2026-04-19 11:04:31 +0400

    solving task 2 Count patterns like co?e

--- src/TASK4.java
+++ src/TASK4.java
@@ -1,13 +1,18 @@
 public class TASK4 {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         String TEXT = "aaacodebbb";
         int count = 0;
-        for (int i = 0 ; i < TEXT.length();i++){
-            if (TEXT.charAt(i))=='c'&& TEXT.charAt(i +1)=='o'&&TEXT.charAt(i+3)=='e'){
-                count++
+        for (int i = 0; i < TEXT.length() - 1; i++) {
+            if (TEXT.charAt(i) == 'c' &&
+                    TEXT.charAt(i + 1) == 'o' &&
+                    TEXT.charAt(i + 3) == 'e') {
+                count++;
 
             }
 
+
         }
+        System.out.println(count);
+
     }
 }
