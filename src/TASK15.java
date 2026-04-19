0fcc2ee85dd2ba2003867c7181d91d3f64da318b
Shahd <shahad9mubarak@gmail.com>
2026-04-19 12:33:36 +0400

    DOING TWO INTEGER THAT TAKES FRIST AND LAST INDEX

---
+++ src/TASK15.java
@@ -0,0 +1,9 @@
+public class TASK15 {
+    static void main(String[] args) {
+
+    }
+    public static String getSandwich(String str){
+        int first = str.indexOf("bread");
+        int last = str.lastIndexOf("bread");
+    }
+}
