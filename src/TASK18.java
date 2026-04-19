ec07ecbedcad5c5746f274775e2006ff602c820b
Shahd <shahad9mubarak@gmail.com>
2026-04-19 14:59:41 +0400

    task 18 Remove middle character from z*p pattern

--- src/TASK18.java
+++ src/TASK18.java
@@ -1,11 +1,20 @@
 public class TASK18 {
     static void main(String[] args) {
+        String str="zipXzap";
+        System.out.println(zipZap(str));
 
     }
     public static String zipZap(String str){
         String RESULT = "";
         for (int i = 0 ; i<str.length();i++){
-            if (i <str.length()-2 && str.charAt(i)=='z' && str.charAt(i+2))
+            if (i <str.length()-2 && str.charAt(i)=='z' && str.charAt(i+2)=='p'){
+                RESULT +="zp";
+                i+=2;
+            }
+            else {
+                RESULT+=str.charAt(i);
+            }
         }
+        return RESULT;
     }
 }
