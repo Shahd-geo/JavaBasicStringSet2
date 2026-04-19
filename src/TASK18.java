975da91ed5be74f8f7e59f76db35ea4d4a6a99a3
Shahd <shahad9mubarak@gmail.com>
2026-04-19 14:56:38 +0400

    start solve task 18

---
+++ src/TASK18.java
@@ -0,0 +1,11 @@
+public class TASK18 {
+    static void main(String[] args) {
+
+    }
+    public static String zipZap(String str){
+        String RESULT = "";
+        for (int i = 0 ; i<str.length();i++){
+            if (i <str.length()-2 && str.charAt(i)=='z' && str.charAt(i+2))
+        }
+    }
+}
