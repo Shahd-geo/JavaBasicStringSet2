1a20396cdca25b26432c8a445ae4acaad8f02940
Shahd <shahad9mubarak@gmail.com>
2026-04-19 10:47:26 +0400

    fix loop length-2 to avoid index error

--- src/TASK3.java
+++ src/TASK3.java
@@ -8,7 +8,7 @@ public class TASK3 {
     public static boolean catdog (String  str){
         int catCount = 0;
         int dogCount = 0;
-        for (int i = 0; i < str.length() - 1; i++) {
+        for (int i = 0; i < str.length() - 2; i++) {
             if (str.substring(i, i + 3).equals("cat")) {
                 catCount++;
             }
