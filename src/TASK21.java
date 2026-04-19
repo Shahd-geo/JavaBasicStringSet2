f6c3f00239c8aa8bdb515023aa8d64fc62fb3c26
Shahd <shahad9mubarak@gmail.com>
2026-04-19 15:35:24 +0400

    task21 Return characters before and after each occurrence of a word

--- src/TASK21.java
+++ src/TASK21.java
@@ -1,5 +1,10 @@
 public class TASK21 {
     static void main(String[] args) {
+        String str="abcXY123XYijk";
+        String word = "XY";
+        System.out.println(wordEnds(str,word));
+
+
 
     }
     public static String wordEnds(String str, String word) {
@@ -9,5 +14,19 @@ public class TASK21 {
         for (int i = 0; i <= str.length() - word.length(); i++) {
 
             if (str.substring(i, i + word.length()).equals(word)) {
+                if (i > 0) {
+                    result += str.charAt(i - 1);
+                }
 
+                if (i + word.length() < str.length()) {
+                    result += str.charAt(i + word.length());
+                }
             }
+        }
+
+        return result;
+    }
+
+}
+
+
