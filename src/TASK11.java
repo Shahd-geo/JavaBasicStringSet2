31e05e5e0d549a58523399279b65c9b29b787d8e
Shahd <shahad9mubarak@gmail.com>
2026-04-19 12:01:14 +0400

    task 11 Build string using decreasing prefixes. .

--- src/TASK11.java
+++ src/TASK11.java
@@ -1,8 +1,17 @@
 public class TASK11 {
     static void main(String[] args) {
+        String str ="Chocolate" ;
+        int n = 4;
+        System.out.println(repeatFront(str,n));
 
     }
-    public static String repeatEnd(String str,Integer n){
-        String
+    public static String repeatFront(String str,int n){
+        String result = "";
+        for (int i = n ; i >0 ; i--){
+            result+=str.substring(0,i);
+            }
+        return result;
+        }
+
     }
-}
+
