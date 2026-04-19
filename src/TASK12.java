64a74c29c0ff02e69d1ad4c7a4f48322784ede98
Shahd <shahad9mubarak@gmail.com>
2026-04-19 12:05:01 +0400

    start solve task 12

---
+++ src/TASK12.java
@@ -0,0 +1,11 @@
+public class TASK12 {
+    static void main(String[] args) {
+
+    }
+    public static String repeatSeparato(String str,String str2,int n){
+        String result = "";
+        for (int i = 0 ; i < str.length()-1 ; i++){
+
+        }
+    }
+}
