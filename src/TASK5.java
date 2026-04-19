9bb7ac752a317df8e39199c847a865fc2bdbc761
Shahd <shahad9mubarak@gmail.com>
2026-04-19 11:11:05 +0400

    bulding java code include ignore case

--- src/TASK5.java
+++ src/TASK5.java
@@ -2,5 +2,7 @@ public class TASK5 {
     static void main(String[] args) {
         String str1 = "Hiabc";
         String str2="abc";
+        boolean result = str1.endsWith(str2) || str2.endsWith(str1);
+
     }
 }
