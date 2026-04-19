13d2b4733ac381f4ec7b49fd5ae94d5a0d3a31ae
Shahd <shahad9mubarak@gmail.com>
2026-04-19 12:10:28 +0400

    task 12 Repeat word with separator

--- src/TASK12.java
+++ src/TASK12.java
@@ -1,11 +1,22 @@
 public class TASK12 {
     static void main(String[] args) {
+        String str = "Word";
+        String sep = "x";
+        int n = 4;
+        System.out.println(repeatSeparato(str, sep, n));
 
     }
-    public static String repeatSeparato(String str,String str2,int n){
+
+    public static String repeatSeparato(String str, String sep, int n) {
         String result = "";
-        for (int i = 0 ; i < str.length()-1 ; i++){
+        for (int i = 0; i < n; i++) {
+            result += str;
+            if (i < n - 1) {
+                result += sep;
+            }
 
         }
+        return result;
     }
 }
+
