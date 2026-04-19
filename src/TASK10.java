6aceab001f163b10a92742912b50bf46ca074230
Shahd <shahad9mubarak@gmail.com>
2026-04-19 11:54:01 +0400

    TASK 10 Repeat the last n characters n times

--- src/TASK10.java
+++ src/TASK10.java
@@ -1,11 +1,16 @@
 public class TASK10 {
     static void main(String[] args) {
+        String str = "Hello";
+        Integer n= 3;
+        System.out.println(repeatEnd(str,n));
 
     }
     public static String repeatEnd(String str,Integer n){
        String end = str.substring(str.length()-n) ;
        String RESULT = "";
-
-
+       for (int i = 0 ; i < n ; i++){
+           RESULT +=end;
+       }
+        return RESULT;
     }
 }
