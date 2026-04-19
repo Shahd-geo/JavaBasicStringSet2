05b6b5cc00abdd0eeee8a1295ff0ea125ca6fc74
Shahd <shahad9mubarak@gmail.com>
2026-04-19 11:37:27 +0400

    doing the loop

--- src/TASK8.java
+++ src/TASK8.java
@@ -1,5 +1,15 @@
 public class TASK8 {
     static void main(String[] args) {
+    String str = "aaxbby";
+    System.out.println(xyBalance(str));
 
+    }
+    public static boolean xyBalance(String str){
+        for (int i = 0 ; i < str.length()-1 ; i++){
+            if (str.contains('x')&& str.contains('y')){
+                return true;
+            }
+        }
+        return false;
     }
 }
