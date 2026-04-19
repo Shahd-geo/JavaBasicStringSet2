2a23db33a08b9e98ec4165e2b5ca0a161da9c898
Shahd <shahad9mubarak@gmail.com>
2026-04-19 12:17:49 +0400

    TASK 13 Check if prefix of length N appears again in string

--- src/TASK13.java
+++ src/TASK13.java
@@ -1,12 +1,21 @@
 public class TASK13 {
     static void main(String[] args) {
+        String str="abXYabc";
+        int n=1;
+        System.out.println(prefixAgain(str,n));
 
     }
     public static boolean prefixAgain(String str,int n){
         String prefix = str.substring(0, n);
 
         for (int i = 1; i <= str.length() - n; i++) {
-
-
+            if (str.substring(i, i + n).equals(prefix)) {
+                return true;
+            }
         }
+
+        return false;
+    }
+
+
 }
